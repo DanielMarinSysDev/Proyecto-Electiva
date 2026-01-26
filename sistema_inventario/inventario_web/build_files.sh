b399362eb9aa7ab0246c6634e02a941c5db5471b
Daniel Marin <danielmarinpacheco01@gmail.com>
2026-01-25 22:15:15 -0400

    Cleanup: Remove superuser script and restore build_files.sh

--- sistema_inventario/inventario_web/build_files.sh
+++ sistema_inventario/inventario_web/build_files.sh
@@ -2,12 +2,4 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-# Asegurar que las migraciones se ejecuten
-echo "Running migrations..."
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate --noinput
-echo "Migrations finished."
-
-# Crear superusuario
-python3.9 create_superuser.py
 echo "Build complete."
