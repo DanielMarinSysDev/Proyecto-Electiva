a7e2c3e7e4b485f9082ade6b479070ee43f7961d
Daniel Marin <danielmarinpacheco01@gmail.com>
2026-01-25 20:55:50 -0400

    Force migrations in build script

--- sistema_inventario/inventario_web/build_files.sh
+++ sistema_inventario/inventario_web/build_files.sh
@@ -2,6 +2,12 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py migrate
+# Asegurar que las migraciones se ejecuten
+echo "Running migrations..."
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate --noinput
+echo "Migrations finished."
+
+# Crear superusuario
 python3.9 create_superuser.py
 echo "Build complete."
