f7eadc9985b92a081293d2d1cbb17cc7f45edf9c
unknown <PSMBNALAB2-PC12@PSMBNALAB2-PC12.PSMBNA.EDU.VE>
2026-01-28 09:41:25 -0400

    Trigger redeploy to apply migrations

--- sistema_inventario/inventario_web/build_files.sh
+++ sistema_inventario/inventario_web/build_files.sh
@@ -2,5 +2,6 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+echo "Running migrations..."
 python3.9 manage.py migrate
 echo "Build complete."
